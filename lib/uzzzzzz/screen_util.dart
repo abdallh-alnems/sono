2de1842408abc38a517b2b7293fb80bb9e9cce2c
abdallh-alnems <abdallhmoustafa295@gmail.com>
2024-05-12 06:55:24 +0300

    error platform web

---
+++ lib/uzzzzzz/screen_util.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ScreenUtil {
+  static ScreenUtil _instance = ScreenUtil._();
+
+  factory ScreenUtil() => _instance;
+
+  ScreenUtil._();
+
+  double setWidth(num width) {
+    // Your implementation for setting width
+    return width.toDouble();
+  }
+
+  double setHeight(num height) {
+    // Your implementation for setting height
+    return height.toDouble();
+  }
+
+  double setSp(num fontSize) {
+    // Your implementation for setting font size
+    return fontSize.toDouble();
+  }
+
+  double radius(num r) {
+    // Your implementation for setting radius
+    return r.toDouble();
+  }
+}
